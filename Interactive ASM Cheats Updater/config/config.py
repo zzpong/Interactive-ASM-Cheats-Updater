e7a3ffc190e98f8da53615f58cb879dc2fe331d3
zzpong <slightzzpong@qq.com>
2023-11-16 16:40:49 +0800

    Add more code type

--- Interactive ASM Cheats Updater/config/config.py
+++ Interactive ASM Cheats Updater/config/config.py
@@ -3,11 +3,20 @@ code_pattern = {
     {
         "code_type_0x0":
         {
-            "pattern": "r'^ 0[12][abcdef\d]{6} *( * [abcdef\d]{8}){0,3} *$'",
+            "pattern": "r'^ *0[12][abcdef\d]{6} *( * [abcdef\d]{8}){2,3} *$'",
             "generate_type": 'flat_generate',
             "description": 'Code Type 0x0: Store Static Value to Memory',
             "details":
                     """[f'Code type 0x0 allows writing a static value to a memory address.']"""
+        },
+        "code_type_0x1":
+        {
+            "pattern": "r'^ *1[1248][0123][123456]00[abcdef\d]{2} *( * [abcdef\d]{8}){2,3} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0x1: Begin Conditional Block',
+            "details":
+                """[f'Code type 0x1 performs a comparison of the contents of memory to a static value.',
+                    'If the condition is not met, all instructions until the appropriate End or Else conditional block terminator are skipped.']"""
         },
         "code_type_0x2":
         {
@@ -18,13 +27,103 @@ code_pattern = {
                 """[f'Code type 0x2 marks the end of a conditional block (started by Code Type 0x1 or Code Type 0x8).',
                     'When an Else is executed, all instructions until the appropriate End conditional block terminator are skipped.']"""
         },
+        "code_type_0x3":
+        {
+            "pattern": "r'^ *3[01]0[abcdef\d]0000 *( * [abcdef\d]{8}){0,1} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0x3: Start/End Loop',
+            "details":
+                """[f'Code type 0x3 allows for iterating in a loop a fixed number of times.']"""
+        },
+        "code_type_0x4":
+        {
+            "pattern": "r'^ *400[abcdef\d]0000 *( * [abcdef\d]{8}){2} *$'",
+            "generate_type": 'force_generate',
+            "description": 'Code Type 0x4: Load Register with Static Value',
+            "details":
+                """[f'Code type 0x4 allows setting a register to a constant value.']"""
+        },
+        "code_type_0x5":
+        {
+            "pattern": "r'^ *5[1248][0123][abcdef\d][01]0[abcdef\d]{2} * [abcdef\d]{8} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0x5: Load Register with Memory Value',
+            "details":
+                """[f'Code type 0x5 allows loading a value from memory into a register, either using a fixed address or by dereferencing the destination register.']"""
+        },
+        "code_type_0x6":
+        {
+            "pattern": "r'^ *6[1248]0[abcdef\d][01][01][abcdef\d]0 *( * [abcdef\d]{8}){2} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0x6: Store Static Value to Register Memory Address',
+            "details":
+                """[f'Code type 0x6 allows writing a fixed value to a memory address specified by a register.']"""
+        },
+        "code_type_0x7":
+        {
+            "pattern": "r'^ *7[1248]0[abcdef\d][01234]000 * [abcdef\d]{8} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0x7: Legacy Arithmetic',
+            "details":
+                """[f'Code type 0x7 allows performing arithmetic on registers.',
+                    'However, it has been deprecated by Code type 0x9, and is only kept for backwards compatibility.']"""
+        },
         "code_type_0x8":
         {
-            "pattern": "r'^ *(8)[abcdef\d]{7} *$'",
+            "pattern": "r'^ *8[abcdef\d]{7} *$'",
             "generate_type": 'flat_generate',
             "description": 'Code Type 0x8: Begin Keypress Conditional Block',
             "details":
                     """[f'Code type 0x8 enters or skips a conditional block based on whether a key combination is pressed.']"""
+        },
+        "code_type_0x9":
+        {
+            "pattern": "r'^ *9[1248][\d][abcdef\d][abcdef\d][01][abcdef\d]0 *( * [abcdef\d]{8}){0,2} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0x9: Perform Arithmetic',
+            "details":
+                """[f'Code type 0x9 allows performing arithmetic on registers.']"""
+        },
+        "code_type_0xA":
+        {
+            "pattern": "r'^ *A[1248][abcdef\d]{2}[01][012345][abcdef\d]{2} *( * [abcdef\d]{8}){0,1} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0xA: Store Register to Memory Address',
+            "details":
+                """[f'Code type 0xA allows writing a register to memory.']"""
+        },
+        "code_type_0xC0":
+        {
+            "pattern": "r'^ *C0[1248][123456][abcdef\d][012345][abcdef\d]{2} *( * [abcdef\d]{8}){0,2} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0xC0: Begin Register Conditional Block',
+            "details":
+                """[f'Code type 0xC0 performs a comparison of the contents of a register and another value.'
+                    'If the condition is not met, all instructions until the appropriate conditional block terminator are skipped.']"""
+        },
+        "code_type_0xC1":
+        {
+            "pattern": "r'^ *C10[abcdef\d]0[abcdef\d][0123]0 *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0xC1: Save or Restore Register',
+            "details":
+                """[f'Code type 0xC1 performs saving or restoring of registers.']"""
+        },
+        "code_type_0xC2":
+        {
+            "pattern": "r'^ *C2[0123]0[abcdef\d]{4} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0xC2: Save or Restore Register with Mask',
+            "details":
+                """[f'Code type 0xC2 performs saving or restoring of multiple registers using a bitmask.']"""
+        },
+        "code_type_0xC3":
+        {
+            "pattern": "r'^ *C3000[078F][0F][abcdef\d] *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0xC3: Read or Write Static Register',
+            "details":
+                """[f'Code type 0xC3 reads or writes a static register with a given register.']"""
         },
         "code_type_unknown":
         {
@@ -45,14 +144,64 @@ code_pattern = {
             "details":
                     """[f'Code type 0x0 会向指定内存地址存入数据。']"""
         },
+        "code_type_0x1":
+        {
+            "pattern": "r'^ *1[1248][0123][123456]00[abcdef\d]{2} *( * [abcdef\d]{8}){2,3} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0x1: 开始循环标志',
+            "details":
+                """[f'Code type 0x1 将特定内存地址数据与某常量比较。',
+                    '如果条件不满足，则跳过此指令至Code type 0x2之间的所有指令。']"""
+        },
         "code_type_0x2":
         {
             "pattern": "r'^ *2[01]000000 *$'",
             "generate_type": 'flat_generate',
-            "description": 'Code Type 0x2: 结束标志',
+            "description": 'Code Type 0x2: 结束循环标志',
             "details":
-                """[f'Code type 0x2 为循环结束标志 (循环始于 Code Type 0x1 或 Code Type 0x8).',
+                """[f'Code type 0x2 为循环结束标志 (循环始于 Code Type 0x1 或 Code Type 0x8)。',
                     '在使用else指令时，此结束标志前的所有代码都将被跳过。']"""
+        },
+        "code_type_0x3":
+        {
+            "pattern": "r'^ *3[01]0[abcdef\d]0000 *( * [abcdef\d]{8}){0,1} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0x3: 开始/结束循环',
+            "details":
+                """[f'Code type 0x3 将执行本指令所指定次数的循环体。']"""
+        },
+        "code_type_0x4":
+        {
+            "pattern": "r'^ *400[abcdef\d]0000 *( * [abcdef\d]{8}){2} *$'",
+            "generate_type": 'force_generate',
+            "description": 'Code Type 0x4: 为寄存器载入某常量',
+            "details":
+                """[f'Code type 0x4 将会为指定寄存器加载某常量。']"""
+        },
+        "code_type_0x5":
+        {
+            "pattern": "r'^ *5[1248][0123][abcdef\d][01]0[abcdef\d]{2} * [abcdef\d]{8} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0x5: 为寄存器载入某内存',
+            "details":
+                """[f'Code type 0x5 将会为指定寄存器加载某内存数据。']"""
+        },
+        "code_type_0x6":
+        {
+            "pattern": "r'^ *6[1248]0[abcdef\d][01][01][abcdef\d]0 *( * [abcdef\d]{8}){2} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0x6: 将常量写入某内存',
+            "details":
+                """[f'Code type 0x6 将会为指定内存写入某寄存器常量数据。']"""
+        },
+        "code_type_0x7":
+        {
+            "pattern": "r'^ *7[1248]0[abcdef\d][01234]000 * [abcdef\d]{8} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0x7: 算术运算（旧版）',
+            "details":
+                """[f'Code type 0x7 为算术运算指令。',
+                    '但此指令已被更新的 Code type 0x9 替代，此处仅为保证后向兼容。']"""
         },
         "code_type_0x8":
         {
@@ -61,6 +210,55 @@ code_pattern = {
             "description": 'Code Type 0x8: 按键触发标志',
             "details":
                     """[f'Code type 0x8 通过指定按键决定某条件单元是否被触发。']"""
+        },
+        "code_type_0x9":
+        {
+            "pattern": "r'^ *9[1248][\d][abcdef\d][abcdef\d][01][abcdef\d]0 *( * [abcdef\d]{8}){0,2} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0x9: 算术运算',
+            "details":
+                """[f'Code type 0x9 允许用户在寄存器上进行算术运算。']"""
+        },
+        "code_type_0xA":
+        {
+            "pattern": "r'^ *A[1248][abcdef\d]{2}[01][012345][abcdef\d]{2} *( * [abcdef\d]{8}){0,1} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0xA: 将寄存器数据写入内存',
+            "details":
+                """[f'Code type 0xA 允许将寄存器数据写入内存。']"""
+        },
+        "code_type_0xC0":
+        {
+            "pattern": "r'^ *C0[1248][123456][abcdef\d][012345][abcdef\d]{2} *( * [abcdef\d]{8}){0,2} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0xC0: 寄存器开始循环标志',
+            "details":
+                """[f'Code type 0xC0 将寄存器值与另一值比较。'
+                    '如果不满足，则跳过之后循环体。']"""
+        },
+        "code_type_0xC1":
+        {
+            "pattern": "r'^ *C10[abcdef\d]0[abcdef\d][0123]0 *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0xC1: 写入寄存器',
+            "details":
+                """[f'Code type 0xC1 提供写入寄存器功能。']"""
+        },
+        "code_type_0xC2":
+        {
+            "pattern": "r'^ *C2[0123]0[abcdef\d]{4} *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0xC2: 写入寄存器（含掩码）',
+            "details":
+                """[f'Code type 0xC2 提供通过掩码同时写入多个寄存器的功能。']"""
+        },
+        "code_type_0xC3":
+        {
+            "pattern": "r'^ *C3000[078F][0F][abcdef\d] *$'",
+            "generate_type": 'flat_generate',
+            "description": 'Code Type 0xC3: 读/写静态寄存器',
+            "details":
+                """[f'Code type 0xC3 将读/写某静态寄存器至指定寄存器。']"""
         },
         "code_type_unknown":
         {
@@ -76,7 +274,7 @@ code_pattern = {
 localization = {
     "loc_EN":
         {
-            "title": "Code Updater for Nintendo Switch ver 1.0.0",
+            "title": "Code Updater for Nintendo Switch ver 1.0.2",
             "wing_length_default": "[1, 1]",
             "extra_wing_length_default": "[2, 2]",
             "hints_map":
@@ -166,7 +364,7 @@ localization = {
         },
  "loc_CN":
          {
-            "title": "金手指自动更新器 ver 1.0.0c",
+            "title": "金手指自动更新器 ver 1.0.1c",
             "wing_length_default": "[1, 1]",
             "extra_wing_length_default": "[2, 2]",
             "hints_map":
