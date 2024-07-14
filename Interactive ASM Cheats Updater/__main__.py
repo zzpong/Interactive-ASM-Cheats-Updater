0b2ed56f2034433735580c1f430c2eed603b33d1
zzpong <slightzzpong@qq.com>
2024-07-14 13:15:10 +0800

    ver1.1.2

--- Interactive ASM Cheats Updater/__main__.py
+++ Interactive ASM Cheats Updater/__main__.py
@@ -1,7 +1,8 @@
+import importlib
 import os
+import sys
 
 from utilities.interface import CodeUpdaterInterface
-from config.config import *
 from utilities.logger import Logger
 
 
@@ -19,14 +20,19 @@ class GlobalInfo:  # Read-ONLY Property
 
         self.logger = Logger(root_path)
 
-        self.title = localization[language]['title']
-        self.hints_map = localization[language]['hints_map']
-        self.btn_map = localization[language]['btn_map']
-        self.msg_map = localization[language]['msg_map']
-        self.wing_length_default = localization[language]['wing_length_default']
-        self.extra_wing_length_default = localization[language]['extra_wing_length_default']
+        sys.path.append(self.root_path)
+        config = importlib.import_module('config')
 
-        self.code_pattern = code_pattern[language]
+        self.title = config.localization[language]['title']
+        self.hints_map = config.localization[language]['hints_map']
+        self.btn_map = config.localization[language]['btn_map']
+        self.msg_map = config.localization[language]['msg_map']
+
+        self.wing_length_default = config.preference['wing_length_default']
+        self.extra_wing_length_default = config.preference['extra_wing_length_default']
+        self.max_shown_address = config.preference['max_shown_address']
+
+        self.code_pattern = config.code_pattern[language]
 
         self.supported_package_type = ['.xci', '.nsp', '.xcz', '.nsz']
 
